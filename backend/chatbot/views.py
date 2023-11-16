a35905dcfdc1adb056c7eb10486a36e07a173ed0
Frank Wang <broodstar_frank@hotmail.com>
2023-11-16 17:59:18 -0500

    added API key management to backend

--- backend/chatbot/views.py
+++ backend/chatbot/views.py
@@ -1,13 +1,24 @@
 from .apikeys.auth import HasAPIKey, APIKeyAuthentication
+from .apikeys.models import APIKey as APIKeyModel
+from rest_framework import status, permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework.authentication import SessionAuthentication
 from openai import OpenAI
 from dotenv import load_dotenv
 load_dotenv()
 
 
 client = OpenAI()
+
+
+class APIKey(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+    authentication_classes = (SessionAuthentication,)
+
+    def post(self, request):
+        api_key, key = APIKeyModel.objects.create_key(user=request.user)
+        return Response({"key": key}, status=status.HTTP_200_OK)
 
 
 class Chat(APIView):
