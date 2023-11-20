b81748c57151c10857bd8a8f0ae276ffdac8fc17
Frank Wang <broodstar_frank@hotmail.com>
2023-11-20 15:46:41 -0500

    modified database setting to use postgresql

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,8 +82,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'chatase',
+        'USER': 'postgres',
+        'PASSWORD': '${PASSWORD}',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
