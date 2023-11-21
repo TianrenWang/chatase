cd6189a218fc7dbc0e1627d0377d7d02b47e5f8c
Frank Wang <broodstar_frank@hotmail.com>
2023-11-21 14:55:12 -0500

    chatbot api can create conversations and track new messages

--- backend/chatbot/models.py
+++ backend/chatbot/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+import uuid
+
+
+class Conversation(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name="conversations",
+    )
+    name = models.CharField(max_length=30)
+
+
+class Message(models.Model):
+    isHuman = models.BooleanField(default=False)
+    text = models.CharField(default="")
+    context = models.CharField(default="")
+    conversation = models.ForeignKey(
+        Conversation,
+        on_delete=models.CASCADE,
+        related_name="messages",
+        null=True
+    )
+    createdAt = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        indexes = [models.Index(fields=["-createdAt"])]
--- backend/chatbot/views.py
+++ backend/chatbot/views.py
@@ -1,5 +1,6 @@
 from .apikeys.auth import HasAPIKey, APIKeyAuthentication
 from .apikeys.models import APIKey as APIKeyModel
+from .models import Conversation, Message
 from rest_framework import status, permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -26,12 +27,46 @@ class Chat(APIView):
     permission_classes = [HasAPIKey]
     authentication_classes = (APIKeyAuthentication,)
 
-    def post(self, request):
-        response = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": "system", "content": "You are a helpful assistant."},
-                {"role": "user", "content": "How are you doing son?"}
-            ]
-        )
+    def get(self, request):
+        if not "conversationId" in request.query_params:
+            return Response({"message": "You must include 'conversationId' in request query."}, status=status.HTTP_400_BAD_REQUEST)
+
+        conversationId = request.query_params["conversationId"]
+
+        key = request.META["HTTP_AUTHORIZATION"].split()[1]
+        try:
+            user = APIKeyModel.objects.get_from_key(key).user
+            conversation = Conversation.objects.get(
+                pk=conversationId, user=user)
+            humanMessage = Message.objects.create(
+                text=request.data["text"], context="", conversation=conversation, isHuman=True)
+            humanMessage.save()
+            response = client.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {"role": "system", "content": "You are a helpful assistant."},
+                    {"role": "user", "content": humanMessage.text}
+                ]
+            )
+            botMessage = Message.objects.create(
+                text=response.choices[0].message.content, context="", conversation=conversation)
+            botMessage.save()
+        except Conversation.DoesNotExist:
+            return Response({"message": f"Conversation with ID {conversationId} does not exist"}, status=status.HTTP_404_NOT_FOUND)
+        except:
+            return Response({"message": "Internal error"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         return Response({"message": response.choices[0].message.content}, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        try:
+            if not "username" in request.data:
+                return Response({"message": "You must provide the 'username' field in the body."}, status=status.HTTP_400_BAD_REQUEST)
+            key = request.META["HTTP_AUTHORIZATION"].split()[1]
+            user = APIKeyModel.objects.get_from_key(key).user
+            conversation = Conversation.objects.create(
+                name=request.data["username"], user=user)
+
+            conversation.save()
+        except:
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response({"conversationId": conversation.id}, status=status.HTTP_200_OK)
